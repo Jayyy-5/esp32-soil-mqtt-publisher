c18fc1803cb7e75a2779dcdeda5be836b81b3ff7
Jayyy-5 <jsenthilkuma@binghamton.edu>
2026-02-02 13:55:39 -0500

    Add files via upload
    Add ESP32 soil MQTT publisher code

---
+++ ESP32_soil_MQTT_Publisher.ino.ino
@@ -0,0 +1,58 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "81 State-407";
+const char* password = "Bearcats1407";
+
+const char* mqtt_server = "broker.mqttdashboard.com";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "ESP32/SOIL/DATA";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void connectWiFi() {
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+}
+
+void connectMQTT() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (client.connect("ESP32_Soil_Client")) {
+      Serial.println("connected!");
+    } else {
+      Serial.println("failed, retrying...");
+      delay(2000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectWiFi();
+  client.setServer(mqtt_server, mqtt_port);
+  connectMQTT();
+}
+
+void loop() {
+  if (!client.connected()) {
+    connectMQTT();
+  }
+  client.loop();
+
+  int soilMoisture = random(30, 80);
+  char msg[10];
+  sprintf(msg, "%d", soilMoisture);
+  client.publish(mqtt_topic, msg);
+
+  Serial.print("Soil Moisture Sent: ");
+  Serial.println(soilMoisture);
+
+  delay(5000);
+}
